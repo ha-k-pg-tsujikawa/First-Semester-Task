d389f9a17b615df7d7ccbc3780f72018e1abfbfa
MSI\tsuji <pokebey63@gmail.com>
2020-08-19 21:44:49 +0900

    課題2動作確認済み

--- マルバツ/マルバツ/Project/src/WinMain.cpp
+++ マルバツ/マルバツ/Project/src/WinMain.cpp
@@ -43,15 +43,16 @@
 // ==============================
 // グローバル変数
 // ==============================
-※※		// 横:STAGE_WIDTH、縦:STAGE_HEIGHTのint型２次元配列 map を宣言
+int map[STAGE_WIDTH][STAGE_HEIGHT];		// 横:STAGE_WIDTH、縦:STAGE_HEIGHTのint型２次元配列 map を宣言
+int EndCount = 0;
 
 // ==============================
 // 関数プロトタイプ宣言
 // ==============================
 // 盤面の x, y の位置に石が置けるかどうか
-※※ // bool型の戻り値、int型の引数x,yを持つIsPutStone関数を宣言
+bool IsPutStone(int x,int y); // bool型の戻り値、int型の引数x,yを持つIsPutStone関数を宣言
 // 勝者が居るかを調べる
-※※ // int型の戻り値を持つCheckWinner関数を宣言
+int CheckWinner(); // int型の戻り値を持つCheckWinner関数を宣言
 
 // ==============================
 // Main関数
@@ -71,10 +72,10 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 
 	// ローカル変数宣言
 	// ----------------------------------------------------
-	※※　整数型の変数pos_xを宣言し、0で初期化		// X座標選択位置
-	※※　整数型の変数pos_yを宣言し、0で初期化		// Y座標選択位置
-	※※　整数型の変数turnを宣言し、STONE_WHITEで初期化	// 現在の手番
-	※※　整数型の変数winnerを宣言し、WINNER_NONで初期化	// 勝利者
+	int pos_x = 0;//※※　整数型の変数pos_xを宣言し、0で初期化		// X座標選択位置
+	int pos_y = 0;//※※　整数型の変数pos_yを宣言し、0で初期化		// Y座標選択位置
+	int turn = STONE_WHITE;//※※　整数型の変数turnを宣言し、STONE_WHITEで初期化	// 現在の手番
+	int winner = WINNER_NON;//※※　整数型の変数winnerを宣言し、WINNER_NONで初期化	// 勝利者
 
 	// 各種初期化処理
 	// ----------------------------------------------------
@@ -82,8 +83,12 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	DrawInit();				// 描画処理初期化関数の呼び出し
 
 	// mapの初期化
-	※※　二次元配列mapの全要素を STONE_MAX で初期化する
-
+	//※※　二次元配列mapの全要素を STONE_MAX で初期化する
+	for (int i = 0; i < STAGE_WIDTH; i++) {
+		for (int j = 0; j < STAGE_HEIGHT; j++) {
+			map[i][j] = STONE_MAX;
+		}
+	}
 	// ゲームのメインループ
 	// 画面を１回表示する毎にwhile分を１回処理する
 	// ----------------------------------------------------
@@ -97,37 +102,62 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 		// 以下、毎フレーム更新する処理
 		// ----------------------------------------------------
 		InputUpdate();			// 入力処理更新関数の呼び出し
-		※※ winner に勝利者情報を代入	// 勝利者のチェック
+		winner = CheckWinner(); //※※ winner に勝利者情報を代入	// 勝利者のチェック
 
 		// --- 入力状況をチェックして、適切な処理を行う
 		// 決着がついてない時だけ入力を受け付けるように if文 でチェックする
-		if( ※※ )
+		if( winner == WINNER_NON )
 		{
 			// 上下左右の入力があった時の処理
-			if( ※※ )
+			if( IsPushKey(MY_INPUT_UP) )
 			{
-				※※ pos_yの値を 1 減らす
+				pos_y--;//※※ pos_yの値を 1 減らす
+				if (pos_y < 0)
+				{
+					pos_y = 0;
+				}
 			}
-			else if( ※※ )
+			else if(IsPushKey(MY_INPUT_DOWN))
 			{
-				※※ pos_yの値を 1 増やす
+				pos_y++;//※※ pos_yの値を 1 増やす
+				if (pos_y > 2)
+				{
+					pos_y = 2;
+				}
 			}
-			else if( ※※ )
+			else if(IsPushKey(MY_INPUT_LEFT))
 			{
-				※※ pos_xの値を 1 減らす
+				pos_x--;//※※ pos_xの値を 1 減らす
+				if (pos_x < 0)
+				{
+					pos_x = 0;
+				}
 			}
-			else if( ※※ )
+			else if(IsPushKey(MY_INPUT_RIGHT))
 			{
-				※※ pos_xの値を 1 増やす
+				pos_x++;//※※ pos_xの値を 1 増やす
+				if (pos_x > 2)
+				{
+					pos_x = 2;
+				}
 			}
 			// 決定(=エンターキー)が押された時の処理
-			else if( ※※ )
+			else if(IsPushKey(MY_INPUT_ENTER))
 			{
 				// 現在の座標が有効か if文 でチェックし、
 				// 結果が true の時、以下の処理を行う
-				if ( ※※ )
+				if ( IsPutStone(pos_x,pos_y) == true )
 				{
-					※※
+					EndCount++;
+					map[pos_x][pos_y] = turn;
+					if (turn == STONE_WHITE)
+					{
+						turn = STONE_BLACK;
+					}
+					else
+					{
+						turn = STONE_WHITE;
+					}
 					// 以下の処理を実装する
 					// 選択されている座標と対応するmap配列の要素へturnの値を代入
 					// 次のターンに回すため、turnの値を変更する
@@ -137,11 +167,23 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 
 		// 以下、描画処理
 		// ----------------------------------------------------
-		※※　// 情報文章を描画
-		※※　// ゲームクリアの文字を描画
-		※※　// 枠線を描画
-		※※　２重for文を使って盤面の石を描画する
-		※※　カーソルを描画
+		DrawInformation(turn); //※※　// 情報文章を描画
+		DrawGameClear(winner);   //※※　// ゲームクリアの文字を描画
+		DrawBgLine();	//※※　// 枠線を描画
+		//※※　２重for文を使って盤面の石を描画する
+		for (int i = 0; i < STAGE_WIDTH; i++) {
+			for (int j = 0; j < STAGE_HEIGHT; j++) {
+				if (map[i][j] == STONE_WHITE)
+				{
+					DrawStone(i, j, STONE_WHITE);
+				}
+				if (map[i][j] == STONE_BLACK)
+				{
+					DrawStone(i, j, STONE_BLACK);
+				}
+			}
+		}
+		DrawCursor(pos_x, pos_y); //※※　カーソルを描画
 
 		// ＤＸライブラリを使う上で、モニターへゲーム画面を表示するためのお約束
 		// 必ずループの最後で呼び出す
@@ -166,7 +208,15 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 // ==============================
 bool IsPutStone( int x, int y )
 {
-	※※　盤面の x, y の位置に石が置けるならtrue,置けないならfalseを返す処理
+	if (map[x][y] == STONE_MAX)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+	//※※　盤面の x, y の位置に石が置けるならtrue,置けないならfalseを返す処理
 }
 
 // ==============================
@@ -174,7 +224,60 @@ bool IsPutStone( int x, int y )
 // ==============================
 int CheckWinner()
 {
-	※※　以下の処理を実装する
+	//※※　以下の処理を実装する
+	if (map[0][0] == map[1][0] && map[0][0] == map[2][0] &&  map[0][0] == STONE_WHITE) {
+		return WINNER_WHITE;
+	}
+	else if (map[0][0] == map[1][0] && map[0][0] == map[2][0] && map[0][0] == STONE_BLACK) {
+		return WINNER_BLACK;		
+	}								
+	else if (map[0][1] == map[1][1] && map[0][1] == map[2][1] && map[0][1] == STONE_WHITE) {
+		return WINNER_WHITE;		
+	}								
+	else if (map[0][1] == map[1][1] && map[0][1] == map[2][1] && map[0][1] == STONE_BLACK) {
+		return WINNER_BLACK;		
+	}								
+	else if (map[0][2] == map[1][2] && map[0][2] == map[2][2] && map[0][2] == STONE_WHITE) {
+		return WINNER_WHITE;		
+	}								
+	else if (map[0][2] == map[1][2] && map[0][2] == map[2][2] && map[0][2] == STONE_BLACK) {
+		return WINNER_BLACK;		
+	}								
+	else if (map[0][0] == map[0][1] && map[0][0] == map[0][2] && map[0][0] == STONE_WHITE) {
+		return WINNER_WHITE;		
+	}								
+	else if (map[0][0] == map[0][1] && map[0][0] == map[0][2] && map[0][0] == STONE_BLACK) {
+		return WINNER_BLACK;		
+	}								
+	else if (map[1][0] == map[1][1] && map[1][0] == map[1][2] && map[1][0] == STONE_WHITE) {
+		return WINNER_WHITE;		
+	}								
+	else if (map[1][0] == map[1][1] && map[1][0] == map[1][2] && map[1][0] == STONE_BLACK) {
+		return WINNER_BLACK;		
+	}								
+	else if (map[2][0] == map[2][1] && map[2][0] == map[2][2] && map[2][0] == STONE_WHITE) {
+		return WINNER_WHITE;		
+	}								
+	else if (map[2][0] == map[2][1] && map[2][0] == map[2][2] && map[2][0] == STONE_BLACK) {
+		return WINNER_BLACK;		
+	}								
+	else if (map[0][0] == map[1][1] && map[0][0] == map[2][2] && map[0][0] == STONE_WHITE) {
+		return WINNER_WHITE;		
+	}								
+	else if (map[0][0] == map[1][1] && map[0][0] == map[2][2] && map[0][0] == STONE_BLACK) {
+		return WINNER_BLACK;		
+	}								
+	else if (map[2][0] == map[1][1] && map[2][0] == map[0][2] && map[2][0] == STONE_WHITE) {
+		return WINNER_WHITE;		
+	}								
+	else if (map[2][0] == map[1][1] && map[2][0] == map[0][2] && map[2][0] == STONE_BLACK) {
+		return WINNER_BLACK;
+	}
+	else if (EndCount >= 9)
+	{
+		return WINNER_DRAW;
+	}
+	return WINNER_NON;
 	// 縦、横、斜めが同じ石かどうかを調べる
 	// STONE_WHITE, STONE_BLACK, STONE_MAXを上手く使いましょう
 
